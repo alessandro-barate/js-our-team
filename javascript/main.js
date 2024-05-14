6cbc8f061c991a475f0b5b7fd4a23dd0c25a4426
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-14 15:39:31 +0200

    [UPD] Milestone 1 done

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,42 @@
 "use strict";
 
 const members = [
-  { nome: "Wayne Barnett", role: "Founder & CEO", image: 'wayne-barnett-founder-ceo.jpg' },
-  { nome: "Angela Caroll", role: "Chief Editor", image: 'angela-caroll-chief-editor.jpg' },
-  { nome: "Walter Gordon", role: "Office Manager", image: 'walter-gordon-office-manager.jpg' },
-  { nome: "Angela Lopez", role: "Social Media Manager", image: 'angela-lopez-social-media-manager.jpg' },
-  { nome: "Scott Estrada", role: "Developer", image: 'scott-estrada-developer.jpg' },
-  { nome: "Barbara Ramos", role: "Grapgic Designer", image: 'barbara-ramos-graphic-designer.jpg' },
+  {
+    nome: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    nome: "Angela Caroll",
+    role: "Chief Editor",
+    image: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    nome: "Walter Gordon",
+    role: "Office Manager",
+    image: "walter-gordon-office-manager.jpg",
+  },
+  {
+    nome: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    nome: "Scott Estrada",
+    role: "Developer",
+    image: "scott-estrada-developer.jpg",
+  },
+  {
+    nome: "Barbara Ramos",
+    role: "Grapgic Designer",
+    image: "barbara-ramos-graphic-designer.jpg",
+  },
 ];
 
-console.log(members.'nome');
+console.log(members);
+console.log(members[0]);
+console.log(members[1]);
+console.log(members[2]);
+console.log(members[3]);
+console.log(members[4]);
+console.log(members[5]);
