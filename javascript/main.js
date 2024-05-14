975d13f923710d3840c0bbadde03fe7775e5924e
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-14 15:26:12 +0200

    [UPD] Milestone 0 done

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const members = [
+  { nome: "Wayne Barnett", role: "Founder & CEO" },
+  { nome: "Angela Caroll", role: "Chief Editor" },
+  { nome: "Walter Gordon", role: "Office Manager" },
+  { nome: "Angela Lopez", role: "Social Media Manager" },
+  { nome: "Scott Estrada", role: "Developer" },
+  { nome: "Barbara Ramos", role: "Grapgic Designer" },
+];
