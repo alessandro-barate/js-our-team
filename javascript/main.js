31e9d6af836ede8af5eeafcf6432a000b09dac9f
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-14 15:33:15 +0200

    [UPD] Property added to the object

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const members = [
-  { nome: "Wayne Barnett", role: "Founder & CEO" },
-  { nome: "Angela Caroll", role: "Chief Editor" },
-  { nome: "Walter Gordon", role: "Office Manager" },
-  { nome: "Angela Lopez", role: "Social Media Manager" },
-  { nome: "Scott Estrada", role: "Developer" },
-  { nome: "Barbara Ramos", role: "Grapgic Designer" },
+  { nome: "Wayne Barnett", role: "Founder & CEO", image: 'wayne-barnett-founder-ceo.jpg' },
+  { nome: "Angela Caroll", role: "Chief Editor", image: 'angela-caroll-chief-editor.jpg' },
+  { nome: "Walter Gordon", role: "Office Manager", image: 'walter-gordon-office-manager.jpg' },
+  { nome: "Angela Lopez", role: "Social Media Manager", image: 'angela-lopez-social-media-manager.jpg' },
+  { nome: "Scott Estrada", role: "Developer", image: 'scott-estrada-developer.jpg' },
+  { nome: "Barbara Ramos", role: "Grapgic Designer", image: 'barbara-ramos-graphic-designer.jpg' },
 ];
+
+console.log(members.'nome');
